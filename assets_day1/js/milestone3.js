d3825c9c660479b7fb97e07d73ba05765be86897
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-27 17:57:08 +0100

    milestone3: loop the array, select the keys values, create a card in the html page with those values with each iteration

--- assets_day1/js/milestone3.js
+++ assets_day1/js/milestone3.js
@@ -14,5 +14,25 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
     // salvo in una variabile i dati della mia risposta
     const result = response.data;
     // stampo in console per essere sicuro che ho preso la risposta in modo corretto
-    console.log(result);  
-  });
+    console.log(result);
+    // creo una variabile di supporto di tipo stringa, vuota
+    let items = "";
+    // utilizzando il metodo forEach(), ciclo nell'array del dato
+    result.forEach((object) => {
+      // ad ogni iterazione, concateno gli elementi html per creare una card, usando però i valori degli oggetti all'interno dell'array
+      items += `
+        <div class="col-4">
+          <div class="card p-4 pb-0 rounded-0">
+            <img class="pin" width="32" src="./assets_day1/img/pin.svg" alt="">
+            <img width="300" src="${object.url}" alt="" class="card-img-top rounded-0">
+            <div class="card-body px-0">
+              <span class="card-subtitle text-body-secondary date">${object.date}</span>
+              <h5 class="card-title">${object.title}</h5>
+            </div>
+          </div>
+        </div>
+      `
+    // modifico il contenuto dell'elemento con le cards
+    rowElement.innerHTML = items;
+    })
+  })
