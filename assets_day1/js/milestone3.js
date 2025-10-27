5224e5c2e2749c7a547185cd51da87700d0bff73
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-27 17:54:05 +0100

    milestone3: call get method with axios, print the response

--- assets_day1/js/milestone3.js
+++ assets_day1/js/milestone3.js
@@ -6,3 +6,13 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfrut
 
 // seleziono l'elemento dalla pagina html
 const rowElement = document.getElementById("grid-element");
+
+// tramite la libreria axios, effettuo una chiamata GET al server per ottenere una risposta
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
+  // se ottengo la risposta positiva
+  .then(response => {
+    // salvo in una variabile i dati della mia risposta
+    const result = response.data;
+    // stampo in console per essere sicuro che ho preso la risposta in modo corretto
+    console.log(result);  
+  });
