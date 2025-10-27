61a89f4c62bfea8d4a467aa8a8411e8229dcdfec
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-27 17:52:39 +0100

    milestone3: link the js file script; select html element

---
+++ assets_day1/js/milestone3.js
@@ -0,0 +1,8 @@
+/* 
+Milestone 3
+
+Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+*/
+
+// seleziono l'elemento dalla pagina html
+const rowElement = document.getElementById("grid-element");
