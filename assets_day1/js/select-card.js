97ed30cec16cffe0e359a51e5ec4d4b4833dc08f
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-28 12:06:48 +0100

    day2-milestone2: add timing function to apply a delay

--- assets_day1/js/select-card.js
+++ assets_day1/js/select-card.js
@@ -9,27 +9,30 @@ Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
 Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
 */
 
+// imposto un timer per permettere alla chiamata AJAX di darmi la risposta prima di andare a selezionare gli elementi cards, altrimenti non l'eventListener no funzionerebbe
+setTimeout(() => {
+  // seleziono gli elementi della DOM
+  const cardsElement = document.querySelectorAll(".cards");
+  //console.log(cardsElement);
+  const overlayElement = document.getElementById("overlay")
+  const closeButtonElement = document.getElementById("close-overlay");
+  
+  // scorro nel nodo delle cards
+  cardsElement.forEach((card) => {
+    // ad ogni card aggiungo un evento click
+    card.addEventListener("click", () => {
+      // rimuovo la classe d-none all'overlay
+      overlayElement.classList.remove("d-none");
+      // aggiungo la classe d-block all'overlay
+      overlayElement.classList.add("d-block");
+    })
+  });
+  
+  // aggiungo un evento click al bottone per "chiudere" l'overlay
+  closeButtonElement.addEventListener("click", () => {
+    // aggiungo nuovamente la classe d-none
+    overlayElement.classList.add("d-none");
+  });
+}, 100);
 
-// seleziono gli elementi della DOM
-const cardsElement = document.querySelectorAll(".cards");
-//console.log(cardsElement);
-const overlayElement = document.getElementById("overlay")
-const closeButtonElement = document.getElementById("close-overlay");
-
-// scorro nel nodo delle cards
-cardsElement.forEach((card) => {
-  // ad ogni card aggiungo un evento click
-  card.addEventListener("click", () => {
-    // rimuovo la classe d-none all'overlay
-    overlayElement.classList.remove("d-none");
-    // aggiungo la classe d-block all'overlay
-    overlayElement.classList.add("d-block");
-  })
-});
-
-// aggiungo un evento click al bottone per "chiudere" l'overlay
-closeButtonElement.addEventListener("click", () => {
-  // aggiungo nuovamente la classe d-none
-  overlayElement.classList.add("d-none");
-});
 
